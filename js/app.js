b713c49da2f51a194b47f8457e4239be298578c8
miliberlin <michelle.liebheit@posteo.de>
2019-10-20 14:18:03 +0900

    adding comments for guidelines

---
+++ js/app.js
@@ -0,0 +1,5 @@
+/*
+- nav menu should dinamically update based on the amount of content that is added to the page.
+- the section actively being viewed should be differentiated in some way.
+- when a user clicks on a navigation item, the item should scroll you to the appropriate section rather than giving you the default jump.
+*/
