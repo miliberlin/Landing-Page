fc40143c7e3324f9442bb96abe3f622bcd9c5e7e
miliberlin <michelle.liebheit@posteo.de>
2019-10-21 18:55:42 +0900

    HTML content + CSS styling

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 The ReadMe file should have non-default text in it that is specific to this project. It doesn’t have to be thorough, but should have some basic information, and use correct markdown.
+
+
+<!-- Each Section has an ID (used for the anchor) and
+a data attribute that will populate the li node.
+Adding more sections will automatically populate nav.
+The first section is set to active class by default -->
+
+
+--- How to add a new item to the navigation
+
+Each Section
--- js/app.js
+++ js/app.js
@@ -1,5 +1,68 @@
 /*
 - nav menu should dinamically update based on the amount of content that is added to the page.
+  --> when a section is added it should appear as a menu item
+
+  1. listen to an event for the navigation to be built.
+  2. place of the nav menu
+  3. where is the text coming from for the nav menu
+
 - the section actively being viewed should be differentiated in some way.
+
 - when a user clicks on a navigation item, the item should scroll you to the appropriate section rather than giving you the default jump.
 */
+
+/**
+ *
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ *
+ * Dependencies: None
+ *
+ * JS Version: ES2015/ES6
+ *
+ * JS Standard: ESlint
+ *
+*/
+
+/**
+ * Define Global Variables
+ *
+*/
+
+
+/**
+ * End Global Variables
+ * Start Helper Functions
+ *
+*/
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ *
+*/
+
+// build the nav
+
+
+// Add class 'active' to section when near top of viewport
+
+
+// Scroll to anchor ID using scrollTO event
+
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+*/
+
+// Build menu
+
+// Scroll to section on link click
+
+// Set sections as active
